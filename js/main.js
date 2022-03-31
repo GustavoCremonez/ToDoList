9dd2040d1cff907cb818766ef4a08f1124f8af6d
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-03-31 15:40:06 -0300

    ToDoList - check task done

--- js/main.js
+++ js/main.js
@@ -5,22 +5,36 @@ const Main = {
 	},
 
 	cacheSelectors: function () {
-		this.checkButtons = document.querySelectorAll('.check')
+		this.$checkButtons = document.querySelectorAll('.check')
+		this.$inputTask = document.querySelector('#inputTask')
 	},
 
 	bindEvents: function () {
 		const self = this 
-		this.checkButtons.forEach(function(button) {
+		this.$checkButtons.forEach(function(button) {
 			button.onclick = self.Events.checkButton_click
 		})
+
+		
+		this.$inputTask.onkeypress = self.Events.$inputTask_keypress
 	},
 
 
 
 	Events:{
-		checkButton_click: function(){
-			
+		checkButton_click: function(e){
+			const li = e.target.parentElement
+			const isDone = li.classList.contains('done')
+
+			if(!isDone){
+				li.classList.add('done')
+				return
+			}
+			li.classList.remove('done')
 		}
+
+
+
 	}
 }
 
