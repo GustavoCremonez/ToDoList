5d020884858f463fb91007614603edfb8f02d3e4
Gustavo Cremonez <aparecido2332001@gmail.com>
2022-03-31 15:46:05 -0300

    ToDoList - add task done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const Main = {
 	cacheSelectors: function () {
 		this.$checkButtons = document.querySelectorAll('.check')
 		this.$inputTask = document.querySelector('#inputTask')
+		this.$list = document.querySelector('#list')
 	},
 
 	bindEvents: function () {
@@ -15,8 +16,8 @@ const Main = {
 			button.onclick = self.Events.checkButton_click
 		})
 
-		
-		this.$inputTask.onkeypress = self.Events.$inputTask_keypress
+
+		this.$inputTask.onkeypress = self.Events.$inputTask_keypress.bind(this)
 	},
 
 
@@ -31,10 +32,26 @@ const Main = {
 				return
 			}
 			li.classList.remove('done')
+		},
+
+		$inputTask_keypress: function(e){
+			
+			const key = e.key
+			const value = e.target.value
+
+			if(key === 'Enter'){
+				this.$list.innerHTML += `
+					<li>
+						<div class="check"></div>
+						<label class="task">
+							${value}
+						</label>
+						<button class="remove"></button>
+					</li>
+				`
+				e.target.value = ''
+			}
 		}
-
-
-
 	}
 }
 
